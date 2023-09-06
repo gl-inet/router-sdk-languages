0b8486d28b82c6c5ec6efd98a9f143ae6a60ea08
GL.iNet-Guanwang.Pan <guanwang.pan@gl-inet.com>
2023-09-06 14:40:52 +0800

    test

--- build-langs.sh
+++ build-langs.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
-# if ! command -v jq &> /dev/null; then
-# echo "jq 未安装，请先安装 jq"
-# exit 1
-# fi
+if ! command -v jq &> /dev/null; then
+echo "jq 未安装，请先安装 jq"
+exit 1
+fi
 
 # cd /
 
 merge_lang(){
-        for path in $(find "./"  -type f -regex ".*/\(en\)\.json$" -print0 | xargs -0 -n1 dirname); do
+  for path in $(find "./"  -type f -regex ".*/\(en\)\.json$" -print0 | xargs -0 -n1 dirname); do
+    files=$(ls $path/*.json)
+    output_folder="./dist"
+    
+    for file in $files; do
+      filename=$(basename $file .json)
 
-                files=$(ls $path/*.json)
-        
-                for file in $files; do
-                      echo  $file
-                done
-	done
+      jq -s add $file > $output_folder/release.$filename.json
+    done
+
+    zip -r $output_folder.zip $output_folder
+  done
 }
 merge_lang
 
