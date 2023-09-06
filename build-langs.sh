df989a67b2d73085fb77839d0c6cd073d87007ab
GL.iNet-Guanwang.Pan <guanwang.pan@gl-inet.com>
2023-09-06 17:18:38 +0800

    test Build

--- build-langs.sh
+++ build-langs.sh
@@ -1,26 +1,32 @@
 #!/bin/bash
 
 if ! command -v jq &> /dev/null; then
-echo "jq 未安装，请先安装 jq"
-exit 1
+  echo "jq 未安装，请先安装 jq"
+  exit 1
 fi
 
-# cd /
 output_folder="./dist"
+rm -rf "$output_folder"
+rm -rf "$output_folder.zip"
 mkdir -p "$output_folder"
 
-merge_lang(){
-  for path in $(find "./"  -type f -regex ".*/\(en\)\.json$" -print0 | xargs -0 -n1 dirname); do
-    files=$(ls $path/*.json)
-    
+merge_lang() {
+  merged_json=""
+  for path in $(find "./" -type f -regex ".*/\(en\)\.json$" -print0 | xargs -0 -n1 dirname); do
+    files=$(ls "$path"/*.json)
     for file in $files; do
-      filename=$(basename $file .json)
-      echo $filename
-      jq -s add $file >  $output_folder/release.$filename.json
+      filename=$(basename "$file" .json)
+      output_file="$output_folder/release.$filename.json"
+      if [ -f "$output_file" ]; then
+        merged_json=$(jq -s 'reduce .[] as $item ({}; . * $item)' "$output_file" "$file")
+      else
+        merged_json=$(jq -s add "$file")
+      fi
+      echo "$merged_json" > "$output_file"
     done
-    echo $(ls $output_folder)
-    zip -r $output_folder.zip $output_folder
   done
+  zip -r "$output_folder.zip" "$output_folder"
 }
+
 merge_lang
 
